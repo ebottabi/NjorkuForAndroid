3257993292f2dbcc15c226e5f1f0dac78876e989
Saga <saga@saga-HP-ProBook-4520s.(none)>
2012-01-24 16:35:40 +0100

    License and Readme added

---
+++ src/cm/ebottabi/NjorkuForAndroidActivity.java
@@ -0,0 +1,13 @@
+package cm.ebottabi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NjorkuForAndroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
